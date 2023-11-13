7141f23d8182f558045e0da2247948371ab36701
Andrew Alsing <andrew32alsing@gmail.com>
2023-11-13 14:40:29 -0800

    completed decode image function

--- steganography.py
+++ steganography.py
@@ -17,7 +17,7 @@ def decode_image(path_to_png):
     TODO: Add docstring and complete implementation.
     """
     # Open the image using PIL:
-    encoded_image = Image.open(path_to_png)
+    encoded_image = Image.open("./encoded-imgs/encoded_sample.png")
 
     # Separate the red channel from the rest of the image:
     red_channel = encoded_image.split()[0]
@@ -26,6 +26,15 @@ def decode_image(path_to_png):
     decoded_image = Image.new("RGB", encoded_image.size)
     pixels = decoded_image.load()
     x_size, y_size = encoded_image.size
+
+    for x in range(x_size):
+        for y in range(y_size):
+            red_pixel = red_channel.getpixel((x, y))
+            lsb = red_pixel % 2
+            if lsb == 1:
+                pixels[x, y] = (255, 255, 255)
+            else:
+                pixels[x, y] = (0, 0, 0)
 
     # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
     print(red_channel)  # Start coding here!
@@ -46,3 +55,8 @@ def write_text(text_to_write):
     TODO: Add docstring and complete implementation.
     """
     pass
+
+decode_image('/encoded-imgs/encoded_sample.png')
+
+
+
